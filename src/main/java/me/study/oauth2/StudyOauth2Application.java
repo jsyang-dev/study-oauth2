33ac2fcd8088962d43fbc1ef32b3d4863cb1d1d0
Jeongsu Yang <mycat83@gmail.com>
2020-08-03 15:18:39 +0900

    Initial commit

---
+++ src/main/java/me/study/oauth2/StudyOauth2Application.java
@@ -0,0 +1,13 @@
+package me.study.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyOauth2Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/study/oauth2/StudyOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
