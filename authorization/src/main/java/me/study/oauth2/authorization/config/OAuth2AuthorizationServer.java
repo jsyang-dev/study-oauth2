7b6df8bad6bb19535ebb9abc9a27a041568e9bf6
Jeongsu Yang <mycat83@gmail.com>
2020-08-05 23:26:45 +0900

    client_credentials 인증 적용

--- authorization/src/main/java/me/study/oauth2/authorization/config/OAuth2AuthorizationServer.java
+++ authorization/src/main/java/me/study/oauth2/authorization/config/OAuth2AuthorizationServer.java
@@ -1,41 +1,52 @@
 package me.study.oauth2.authorization.config;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
-@RequiredArgsConstructor
 public class OAuth2AuthorizationServer extends AuthorizationServerConfigurerAdapter {
-
-    private final BCryptPasswordEncoder passwordEncoder;
-
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer security) {
-
-        security
-                .tokenKeyAccess("permitAll()")
-                .checkTokenAccess("isAuthenticated()")
-                .allowFormAuthenticationForClients();
-    }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 
         clients
                 .inMemory()
-                .withClient("testClient").secret(passwordEncoder.encode("123456"))
-                .authorizedGrantTypes("password", "authorization_code", "refresh_token")
-                .authorities("READ_ONLY_CLIENT")
-                .scopes("read_profile_info")
-                .resourceIds("oauth2-resource")
-                .redirectUris("http://localhost:8080/callback")
-                .accessTokenValiditySeconds(30000)
-                .refreshTokenValiditySeconds(240000);
+                .withClient("clientApi")
+                .secret(passwordEncoder().encode("123456"))
+                .authorities("ROLE_API")
+                .scopes("all")
+                .authorizedGrantTypes("client_credentials")
+                .accessTokenValiditySeconds(240000)
+                .and()
+                .withClient("clientAuth")
+                .secret(passwordEncoder().encode("123456"))
+                .authorities("ROLE_OAUTH")
+                .scopes("all")
+                .authorizedGrantTypes("client_credentials")
+                .accessTokenValiditySeconds(240000);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) {
+        security.checkTokenAccess("hasAuthority('ROLE_OAUTH')");
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(4);
     }
 }
--- resource/src/main/java/me/study/oauth2/resource/config/OAuth2ResourceServer.java
+++ resource/src/main/java/me/study/oauth2/resource/config/OAuth2ResourceServer.java
@@ -1,9 +1,12 @@
 package me.study.oauth2.resource.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 
 @Configuration
 @EnableResourceServer
@@ -11,9 +14,19 @@ public class OAuth2ResourceServer extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
         http
                 .authorizeRequests()
-                .antMatchers("/api/**").authenticated()
-                .antMatchers("/").permitAll();
+                .antMatchers("/api/**").hasAuthority("ROLE_API");
+    }
+
+    @Bean
+    public ResourceServerTokenServices resourceServerTokenServices() {
+
+        RemoteTokenServices remoteTokenServices = new RemoteTokenServices();
+        remoteTokenServices.setClientId("clientAuth");
+        remoteTokenServices.setClientSecret("123456");
+        remoteTokenServices.setCheckTokenEndpointUrl("http://localhost:8100/oauth/check_token");
+        return remoteTokenServices;
     }
 }
